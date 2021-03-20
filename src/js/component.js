6d2f0f26038b6aa180aa43406d97eb9cf1a22df3
EricLin252 <jamielin61207@gmail.com>
2021-03-21 01:40:53 +0800

    add comment

--- src/js/component.js
+++ src/js/component.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react';
 import "../css/component.css"
 import db from "./database"
 
+//Block: 景點欄位
 class Block extends Component{
+	//state.openContent: 紀錄block的content開啟狀態
+	//props.spotName: 此景點的名字
+	//props.content: 此景點的詳述
 	constructor(props){
 		super(props);
 		this.setContent = this.setContent.bind(this);
@@ -12,19 +16,23 @@ class Block extends Component{
 		}
 	}
 
+	//setDropdown: 控制block的content開啟
 	setDropdown(){
 		this.setState({
 			openContent: !this.state.openContent
 		});
 	}
 
+	//setContent: 根據此景點是否存在Description，決定是否產生content區塊
 	setContent(){
 		let output = [];
 		if(this.props.content){
 			output.push(
+				//content開啟圖示
 				<div key="dropdown" className="dropdownBtn">{(this.state.openContent)? "▲":"▼"}</div>
 			);
 			output.push(
+				//content
 				<div key="content" className={"blockContent" + (this.state.openContent? " showContent":"")}>{this.props.content}</div>
 			);
 		}
@@ -41,7 +49,10 @@ class Block extends Component{
 	}
 }
 
+//List: 整個景點列表
 class List extends Component{
+	//state.loading: 紀錄目前資料庫存取情況(true: 正在讀取, false: 讀取完成)
+	//props.selectCity: 紀錄目前列表應顯示都市名("": 所有都市)
 	constructor(props){
 		super(props);
 		this.checkScroll = this.checkScroll.bind(this);
@@ -51,6 +62,7 @@ class List extends Component{
 		}
 	}
 
+	//checkScroll: 每次List滑動時呼叫，確認是否滑到底了
 	checkScroll(event){
 		let el = event.currentTarget;
 		let scrollTop = el.scrollTop;
@@ -58,17 +70,21 @@ class List extends Component{
 		let scrollHeight = el.scrollHeight;
 
 		if(scrollHeight - (windowHeight + scrollTop) < 1){
+			//確定已滑到底，將狀態設為讀取中
 			this.setState({
 				loading: true
 			});
 		}
 	}
 
+	//componentDidMount: 網頁初始讀入後，需要讀取一次資料
 	componentDidMount(){
 		this.loadData();
 	}
 
+	//componentDidUpdate: 網頁重新渲染，決定是否新讀取資料
 	componentDidUpdate(prevProps, prevState){
+		//當更改選取都市，將資料清空，將列表滑動至頂部，並設為讀取中狀態
 		if(prevProps.selectCity !== this.props.selectCity){
 			db.clearData();
 			document.getElementById("list").scrollTop = 0;
@@ -76,14 +92,18 @@ class List extends Component{
 				loading: true
 			});
 		}
+		//當loading從false更改為true時，主動更新資料
 		else if(prevState.loading !== this.state.loading){
 			if(prevState.loading === false) this.loadData();
 		}
 	}
 
+	//loadData: 讀取資料，讀取完成後將loading設為false
 	loadData(){
+		//從選取都市名中，新多讀取30筆
 		db.fetchData(30, this.props.selectCity)
 		.then((val) => {
+			//val: 讀取結果("Success": 成功讀取, "Failed": 讀取失敗, "DataEnd": 此都市沒有更多資料了)
 			console.log(val);
 			this.setState({
 				loading: false
@@ -91,6 +111,7 @@ class List extends Component{
 		});
 	}
 
+	//setBlocks: 根據目前讀取入的資料產生每個景點的block
 	setBlocks(){
 		let data = db.getData();
 		console.log(data);
@@ -116,6 +137,7 @@ class List extends Component{
 }
 
 class Page extends Component{
+	//state.selectCity: 紀錄現在選擇顯示哪個都市
 	constructor(){
 		super();
 		this.changeSelectCity = this.changeSelectCity.bind(this);
@@ -125,6 +147,7 @@ class Page extends Component{
 		}
 	}
 
+	//changeSelectCity: 更改所選擇的都市
 	changeSelectCity(event){
 		console.log("change city");
 		this.setState({
@@ -132,6 +155,7 @@ class Page extends Component{
 		});
 	}
 
+	//setCity: 使用db.citySet產生都市選單
 	setCity(){
 		let output = [];
 		for(let key in db.citySet){
--- src/js/database.js
+++ src/js/database.js
@@ -1,7 +1,10 @@
 import jsSHA from "jssha";
 
+//data: 目前已載入的景點資料array
 var data = [];
+//dataEnd: 紀錄目前是否已讀到資料的結尾
 var dataEnd = false;
+//citySet: 所有可選都市的 (中文: request key) 對應集合
 const citySet = {
 	"所有城市": "",
 	"臺北市": "Taipei",
@@ -28,6 +31,7 @@ const citySet = {
 	"連江縣": "LienchiangCounty"
 };
 
+//GetAuthorizationHeader: 產生fetch所需的header
 const GetAuthorizationHeader = () => {
 	let AppID = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
 	let AppKey = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
@@ -41,10 +45,15 @@ const GetAuthorizationHeader = () => {
 	return { 'Authorization': Authorization, 'X-Date': GMTString, 'Accept-Encoding': 'gzip'};
 }
 
+//fetchData: 根據所需資料筆數與都市名fetch資料，並加入data尾端，return回一個promise
+//amt: 此次需要載入的資料筆數
+//city: 此次要載入的目標都市(若為所有都市則為"")
 const fetchData = (amt, city) => {
 	return new Promise((resolve, reject) => {
-		if(dataEnd) resolve("dataEnd");
+		//若已經沒有更多資料能讀了，直接return一個resolve promise
+		if(dataEnd) resolve("DataEnd");
 
+		//將參數結合入request中
 		if(city !== "") city = "/" + city;
 		let requestURL	= "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot"
 		requestURL		+= city
@@ -54,6 +63,7 @@ const fetchData = (amt, city) => {
 		requestURL		+= "&$format=JSON";
 		console.log(requestURL);
 
+		//開始fetch
 		fetch(
 			requestURL,
 			{
@@ -62,12 +72,15 @@ const fetchData = (amt, city) => {
 			}
 		)
 		.then(result => {
+			//檢查連線是否有問題，若無問題，就轉換為json格式
 			if(!result.ok) throw new Error(result.statusText);
 			return result.json();
 		})
 		.then(result => {
 			console.log("receive");
+			//將新獲取的資料append進data尾端
 			data = data.concat(result);
+			//若獲取資料筆數比要求的原筆數少，表示已至結尾
 			if(result.length < amt) dataEnd = true;
 			resolve("Success");
 		})
@@ -78,21 +91,18 @@ const fetchData = (amt, city) => {
 	});
 }
 
+//getData: 返回目前已讀取資料
 const getData = () => {
 	return data;
 }
-
-const getDataLen = () => {
-	return data.length;
-}
-
+//clearData: 清除所有已讀取資料
 const clearData = () => {
 	data = [];
 	dataEnd = false;
 }
-
+//isDataEnd: 返回dataEnd
 const isDataEnd = () => {
 	return dataEnd;
 }
 
-export default {citySet, getData, getDataLen, fetchData, clearData, isDataEnd}
+export default {citySet, getData, fetchData, clearData, isDataEnd}
